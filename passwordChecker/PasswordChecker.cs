af9e520a50f8ed0be80b7ab16ac8688f788c42fe
kucanw <78934647+kucanw@users.noreply.github.com>
2021-09-27 16:07:43 +0200

    Password checker
    Check if the password has at least one upper case letter and one symbol. Updated.

---
+++ passwordChecker/PasswordChecker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string password = Console.ReadLine();
+            int passwordLength = password.Length;
+            int hasSymbols;
+            bool hasUpperCaseLetters = false;
+            
+
+            //Handles upper case checking in password string
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsUpper(password[i]))
+                {
+                    hasUpperCaseLetters = true;
+                }
+            }
+            if (!hasUpperCaseLetters)
+            {
+                Console.WriteLine("Your password doesn't contain uppercase letters");
+            }
+
+            //Checks for symbols in the password
+            hasSymbols = password.IndexOf("*");
+            if (hasSymbols == -1)
+            {
+                Console.WriteLine("Error! Your password has to include symbols");
+            }           
+
+            //Checks if the password is good
+            if(hasSymbols >= 0 && hasUpperCaseLetters)
+            {
+                Console.WriteLine("Password is good");
+            }
+           
+            Console.ReadLine();
+
+
+        }
+    }
+}
